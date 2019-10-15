166e39519d25b7c0b09664061c115f80e47f1a1f
jung-jaehun <gjs4173@gmail.com>
2019-10-15 19:12:37 +0900

    BUTTON_PULLUP

--- basic/01.digital_write/Button/Button.ino
+++ basic/01.digital_write/Button/Button.ino
@@ -11,7 +11,7 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
@@ -19,7 +19,7 @@ void loop() {
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  if (buttonState == LOW) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
   } else {
